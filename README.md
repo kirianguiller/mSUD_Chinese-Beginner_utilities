b2882c1841d22d7e4ccefad7178a33edc40f632d
kirianguiller <kirianguiller@gmail.com>
2023-05-16 12:55:21 +0200

    add: try_pattern_finder

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SUD_Chinese-Beginner_utilities
+
+## Grewpy
+### Install Grewpy
+Follow this documentation to install Grewpy : https://grew.fr/usage/python/
+
+### Use Grewpy
+Follow this documentation to use Grewpy : https://grew.fr/python/
---
+++ scripts/try_pattern_finder.py
@@ -0,0 +1,19 @@
+import sys,os
+
+from grewpy import Corpus, GRSDraft, Rule, Request, Commands, GRS, set_config
+
+set_config("sud")
+
+PATH_CONLL_A1 = "../SUD_Chinese-Beginner/chinese-beginner.A1.mSUD.conllu"
+
+corpus = Corpus(PATH_CONLL_A1)
+graph = corpus[0]
+print(graph)
+
+PATTERN = """
+GOV -[mod]-> DEP; GOV.form = "有"; DEP.form = "没"
+"""
+PATTERN = """GOV -[mod]-> DEP; GOV[upos = VERB|AUX]; DEP.form = "不"; GOV -[1=comp]-> OBJ; OBJ>>GOV"""
+results = corpus.search(Request(PATTERN))
+
+print(len(results))
