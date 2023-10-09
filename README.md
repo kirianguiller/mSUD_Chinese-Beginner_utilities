4205a1e2e908b445e134f27576c59e4c49eec4b7
kirianguiller <kiriangui@gmail.com>
2023-10-09 19:20:34 +0200

    add: morphemic extenson conversion one-shot script

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # SUD_Chinese-Beginner_utilities
 
+Utilities scripts for https://github.com/surfacesyntacticud/SUD_Chinese-Beginner
+
 ## Grewpy
 ### Install Grewpy
 Follow this documentation to install Grewpy : https://grew.fr/usage/python/
---
+++ scripts/one_shots/convert_morphem_prefix.py
@@ -0,0 +1,17 @@
+import os
+from conllup.conllup import readConlluFile, writeConlluFile
+
+PATH_CONLL_FOLDERS = 'SUD_Chinese-Beginner/'
+
+for file in os.listdir(PATH_CONLL_FOLDERS):
+    path_file = os.path.join(PATH_CONLL_FOLDERS, file)
+    print("KK file", path_file)
+    if os.path.isfile(path_file) and file.endswith('.conllu'): 
+        sentences = readConlluFile(path_file)
+        for sentence in sentences:
+            for token in sentence['treeJson']['nodesJson'].values():
+                token["DEPREL"] = token["DEPREL"].replace("@m", "/m")
+                print(token["DEPREL"])
+
+        writeConlluFile(path_file, sentences, overwrite=True)
+            
