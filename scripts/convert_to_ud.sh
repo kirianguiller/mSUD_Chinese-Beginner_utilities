60a498186e87b8e5dd1306420ce8bbca1a28f308
kirianguiller <kiriangui@gmail.com>
2023-10-30 15:50:58 +0100

    add: convert to ud helper bash script

---
+++ scripts/convert_to_ud.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# The name of your file
+OUTPUT_FILENAME="zh_beginner-ud-test.conllu"
+PATH_CONLLUS_FOLDER="./mSUD_Chinese-Beginner"
+PATH_GREW_GRS="../tools/converter/grs/zh_mSUD_to_UD.grs"
+# Create the file if it doesn't exist
+touch "$OUTPUT_FILENAME"
+
+# Empty the file
+> "$OUTPUT_FILENAME"
+
+
+# Levels
+LEVELS=("A1" "A2" "B1" "B2" "C1")
+for LEVEL in "${LEVELS[@]}"; do
+    # Construct the filename for each level
+    INPUT_FILENAME="$PATH_CONLLUS_FOLDER/chinese-beginner.$LEVEL.mSUD.conllu"
+    opam exec -- grew transform -grs "$PATH_GREW_GRS" -config sud -i "$INPUT_FILENAME" -strat zh_SUD_to_UD_main >> "$OUTPUT_FILENAME"
+done
